1a8fc4a738d2e96b908ea066574451d84e20380e
German Bertea <gerbertea@gmail.com>
2023-07-13 12:25:06 +0200

    lessons until 29

--- seccion2/blockchain.py
+++ seccion2/blockchain.py
@@ -1,19 +1,20 @@
-blockchain = [[1]]
+blockchain = []
 
 
 def get_last_blockchain_value():
     return blockchain[-1]
 
 
-def add_value(item):
-    blockchain.append([get_last_blockchain_value(), item])
+def add_value(item, last_transaction=[1]):
+    blockchain.append([last_transaction, item])
 
 
 def print_blockchain():
     print(blockchain)
 
 
-add_value(5.2)
-add_value(5.4)
+add_value(5.90)
+add_value(5.2, get_last_blockchain_value())
+add_value(5.4, get_last_blockchain_value())
 
 print_blockchain()
