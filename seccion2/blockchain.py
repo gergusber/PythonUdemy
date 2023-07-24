0027ea1a73d5cef4ef94d0421c3e324ed94afed2
German Bertea <gerbertea@gmail.com>
2023-07-24 12:18:42 +0200

    add hash alg

--- seccion2/blockchain.py
+++ seccion2/blockchain.py
@@ -1,4 +1,9 @@
-blockchain = []
+genesis_block = {
+    'previous_hash': '',
+    'index': 0,
+    'transactions': []
+}
+blockchain = [genesis_block]
 open_transactions = []
 owner = 'German'
 
@@ -10,7 +15,7 @@ def get_last_blockchain_value():
     return blockchain[-1]
 
 
-def add_transaction(recipient, sender = owner, amount=1.0):
+def add_transaction(recipient, sender= owner, amount= 1.0):
     """Append new value as well as the last blockchain value to the block
 
     Arguments:
@@ -60,7 +65,19 @@ def verify_chain():
 
 
 def mine_block():
-    pass
+    last_block = blockchain[-1]
+    hash_block = ''
+    for key in last_block:
+        value = last_block[key]
+        hash_block = hash_block + str(value)
+
+    print(hash_block)
+    block = {
+        'previous_hash': 'xyz',
+        'index': len(blockchain),
+        'transactions': open_transactions
+    }
+    blockchain.append(block)
 
 
 while True:
@@ -68,6 +85,8 @@ while True:
     print('1- for add new transaction value')
     print('2- for output blockchain blocks')
     print('3- for break')
+    print('4- mine new block')
+
     user_choice = get_user_choice()
 
     if user_choice == '1':
@@ -79,14 +98,13 @@ while True:
         print(open_transactions)
     elif user_choice == '2':
         print_blockchain()
+    elif user_choice == '4':
+        mine_block()
     elif user_choice == 'q':
         break
     else:
         print('input is invalid')
 
-    if not verify_chain():
-        print('Invalid Blockchain')
-        break
 else:
     print('User left')
 
